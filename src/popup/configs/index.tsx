f6dbca4494188a152a5d48efc918c5fbe4ee618c
Hazo Baikulov <spacehaz@gmail.com>
2025-08-10 22:40:05 +0200

    added updates for api urls

--- src/popup/configs/index.tsx
+++ src/popup/configs/index.tsx
@@ -2,9 +2,9 @@ const ADVANCED_STATUS_POINTS = 10
 const REGISTRY = '0xe6912badd4bf0194986c11cbb67876c4bfc0215f'
 const EXTENSION_ID = 'khmegmplfgkfcedomliajfjgkdafcdnh'
 const SEMAPHORE_SUBGRAPH_URL = 'https://api.goldsky.com/api/public/project_cm8g2ca7b0ewq01uea1xl12vp/subgraphs/semaphore-base-sepolia/1.0.0/gn'
-const TASK_MANAGER_API = 'https://zkbring-task-manager-5000565ab7bd.herokuapp.com'
+const TASK_MANAGER_API = 'https://task-manager-production-4062.up.railway.app'
 const VERIFIER_API = 'https://zkbring-tlsn-production.up.railway.app'
-const INDEXER_API = 'https://semaphore-indexer-854648a38846.herokuapp.com'
+const INDEXER_API = 'https://semaphor-indexer-production.up.railway.app'
 const CONNECT_WALLET_URL = 'https://extension-verifications.vercel.app/get-started/connect'
 
 export default {
