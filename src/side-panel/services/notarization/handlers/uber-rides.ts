1cad374c0555d113864d9038dc07751034a40545
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2025-09-29 21:45:04 +0800

    hide cookies from notary during mpc-tls verification

--- src/side-panel/services/notarization/handlers/uber-rides.ts
+++ src/side-panel/services/notarization/handlers/uber-rides.ts
@@ -51,8 +51,11 @@ export class NotarizationUberRides extends NotarizationBase {
       }
       const [transcript, message] = result;
 
+      // keep only HTTP method and URL and hide everything after in the response
+      const sentEnd = `${log[0].method} ${log[0].url}`.length
+
       const commit: Commit = {
-        sent: [{ start: 0, end: transcript.sent.length }],
+        sent: [{ start: 0, end: sentEnd }],
         recv: [],
       };
       console.log(
