e26f65edc9086df68490206fb927aa691601cd4d
Hazo Baikulov <spacehaz@gmail.com>
2025-10-17 17:06:51 +0300

    updates for manifest and configs

--- src/configs/index.tsx
+++ src/configs/index.tsx
@@ -1,4 +1,4 @@
-const REGISTRY = '0x0b2Ab187a6FD2d2F05fACc158611838c284E3a9c';
+const REGISTRY = '0xFEA4133236B093eC727286473286A45c5d4443BC';
 
 // sepolia
 // const REGISTRY = '0x0b2Ab187a6FD2d2F05fACc158611838c284E3a9c'
@@ -9,9 +9,9 @@ const ZUPLO_API_URL = 'https://api.bringid.org';
 const CONNECTOR_HOST = 'app.bringid.org';
 const CONNECT_WALLET_URL = `https://${CONNECTOR_HOST}`;
 const TELEGRAM_URL = 'https://t.me/bringid_chat';
-// const CHAIN_ID = '8453';
+const CHAIN_ID = '8453';
 // sepolia
-const CHAIN_ID = '84532';
+// const CHAIN_ID = '84532';
 
 export default {
   REGISTRY,
